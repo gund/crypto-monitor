3e05bfdc821ebc3890a38d6ae6c64317c6e0ffa3
Alex Malkevich <malkevich.alex@gmail.com>
2024-03-20 00:57:00 +0100

    fix(url-monitor): catch http errors and reject streams

--- libs/url-monitor/src/lib/fetch-url-monitor.ts
+++ libs/url-monitor/src/lib/fetch-url-monitor.ts
@@ -52,20 +52,32 @@ export class FetchUrlMonitor implements UrlMonitor {
 
   protected poll<T>(data: UrlMonitorStartData<T>): Observable<T>;
   protected poll(data: UrlMonitorStartData<unknown>) {
-    const request = new Request(data.url, data);
-    const selector = data.selector ?? ((res) => of(res));
+    const req = new Request(data.url, data);
+    const selector = async (res: Response) => {
+      if (!res.ok) {
+        const text = await res.text();
+        return throwError(
+          () =>
+            new Error(
+              `Request ${req.method} ${req.url} failed with status ${res.status}: ${text}`,
+            ),
+        );
+      }
+
+      return data.selector?.(res) ?? of(res);
+    };
     const polling$ =
       data.pollIntervalMs !== undefined
         ? interval(data.pollIntervalMs)
         : this.polling$;
 
     const wrapError = catchError((e) =>
-      throwError(() => new UrlMonitorPollingError(request, e)),
+      throwError(() => new UrlMonitorPollingError(req, e)),
     );
 
     return polling$.pipe(
       startWith(0),
-      switchMap(() => fromFetch(request, { selector }).pipe(wrapError)),
+      switchMap(() => fromFetch(req, { selector }).pipe(wrapError)),
     );
   }
 }
