d86fcaf115213fb2e928ea6564891d24d68a9d07
Alex Malkevich <malkevich.alex@gmail.com>
2025-01-02 06:31:23 +0100

    fix(monitor-api): remove deprecated endpoints

--- apps/monitor-api/src/app/app.module.ts
+++ apps/monitor-api/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { RouterModule } from '@nestjs/core';
 import { DeprecationModule } from './deprecation';
 import { SaucerSwapModule } from './saucer-swap';
-import { SaucerSwapLPPV1Controller } from './saucer-swap/lpp-v1.controller';
 
 @Module({
   imports: [
@@ -12,6 +11,5 @@ import { SaucerSwapLPPV1Controller } from './saucer-swap/lpp-v1.controller';
     SaucerSwapModule,
     RouterModule.register([{ path: 'saucer-swap', module: SaucerSwapModule }]),
   ],
-  controllers: [SaucerSwapLPPV1Controller],
 })
 export class AppModule {}
